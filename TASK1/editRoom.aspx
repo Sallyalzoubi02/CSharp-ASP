746261b9dc62ac66d894e6b7e218207019f1c862
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-01-29 13:02:19 +0300

    last edit

--- TASK1/editRoom.aspx
+++ TASK1/editRoom.aspx
@@ -3,12 +3,57 @@
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head runat="server">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"/>
+    
     <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Room name</th>
+                        <th scope="col">location</th>
+                        <th scope="col">capacity</th>
+                    </tr>
+                </thead>
+                <tbody id="TableBody" runat="server">
+                </tbody>
+            </table>
+
+            <div class="container bg-primary d-flex flex-column align-items-center justify-content-center w-50 mt-5 p-5">
+                <h1>Search by ID</h1>
+                <label>room id:</label>
+                <asp:TextBox ID="RoomId" runat="server"></asp:TextBox>
+                <asp:Button CssClass="btn btn-light mt-5" ID="serchBtn" runat="server" Text="search" onClick="serchBtn_Click"/>
+                <asp:Label ID="searchRes" runat="server">Result</asp:Label>
+            </div>
+
+            <div class="container bg-primary d-flex flex-column align-items-center justify-content-center w-50 mt-5 p-5">
+                
+                <br />
+                <br />
+                <label>room name:</label>
+                <asp:TextBox ID="roomName" runat="server"></asp:TextBox>
+                <br />
+                <br />
+                <label>room location:</label>
+                <asp:TextBox ID="roomLoc" runat="server"></asp:TextBox>
+                <br />
+                <br />
+                <label>room capacity:</label>
+                <asp:TextBox ID="roomCap" runat="server"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Button CssClass="btn btn-light" ID="edit" runat="server" Text="edit book" OnClick="edit_Click" />
+                <asp:Label ID="res" runat="server" Visible="false"></asp:Label>
+
+            </div>
         </div>
     </form>
 </body>
--- TASK1/editRoom.aspx.cs
+++ TASK1/editRoom.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
 
 namespace TASK1
 {
@@ -11,7 +14,93 @@ namespace TASK1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadBookData();
+            }
+            
+        }
 
+        protected void edit_Click(object sender, EventArgs e)
+        {
+            string filePath = Server.MapPath("rooms.txt");
+            if (File.Exists(filePath))
+            {
+                var fileContent = File.ReadAllLines(filePath);
+
+                for (int i = 0; i < fileContent.Length; i++)
+                {
+                    string[] room = fileContent[i].Split(',');
+
+                    if (room[0] == RoomId.Text)
+                    {
+
+                        room[1] = roomName.Text;
+                        room[2] = roomLoc.Text;
+                        room[3] = roomCap.Text;
+                        fileContent[i] = $"{room[0]} , {room[1]} , {room[2]} , {room[3]} ";
+                        break;
+                    }
+
+                }
+
+                File.WriteAllLines(filePath, fileContent);
+                LoadBookData();
+            }
+        }
+
+        protected void serchBtn_Click(object sender, EventArgs e)
+        {
+
+
+
+            string filePath = Server.MapPath("rooms.txt");
+
+            if (File.Exists(filePath))
+            {
+                string[] fileContent = File.ReadAllLines(filePath);
+                foreach (var line in fileContent)
+                {
+                    string[] room = line.Split(',');
+                    if (room[0] == RoomId.Text)
+                    {
+                        searchRes.Text += $":    {line} ";
+                       
+
+                        return;
+                    }
+
+                }
+            }
+        }
+        private void LoadBookData()
+        {
+            string filePath = Server.MapPath("rooms.txt");
+            if (File.Exists(filePath))
+            {
+                var fileContent = File.ReadAllLines(filePath);
+                StringBuilder tableRows = new StringBuilder();
+
+                foreach (var line in fileContent)
+                {
+                    var columns = line.Split(',');
+                    if (columns.Length == 4)
+                    {
+                        tableRows.Append("<tr>");
+                        tableRows.AppendFormat("<td>{0}</td>", columns[0]);
+                        tableRows.AppendFormat("<td>{0}</td>", columns[1]);
+                        tableRows.AppendFormat("<td>{0}</td>", columns[2]);
+                        tableRows.AppendFormat("<td>{0}</td>", columns[3]);
+                        tableRows.Append("</tr>");
+                    }
+                }
+
+                TableBody.InnerHtml = tableRows.ToString();
+            }
+            else
+            {
+                TableBody.InnerHtml = "<tr><td colspan='4'>File not found.</td></tr>";
+            }
         }
     }
 }
